c15c5c112dbca809956972ec0486e462fed44063
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-03-25 21:13:33 -0300

    header

---
+++ src/components/ui/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css"
+
+export const Header = () => {
+    return (
+        <div>
+            <div>
+                <h2>Kairos</h2>
+            </div>
+        </div>
+    )
+}
