71898ebd740e5f7750f7591a09b532c1ea6aff93
OrnelaCondori <ornelasaracamilacondori@gmail.com>
2025-04-03 13:57:55 -0300

    modal para agregar tareas

---
+++ src/components/ui/Modal/ModalTareas.tsx
@@ -0,0 +1,13 @@
+import { ITarea } from "../../../types/IInterfaces"
+
+const initialState : ITarea = {
+    titulo: "",
+    descripcion: "",
+    estado: "",
+    fechaLimite: "",
+}
+export const ModalTareas = () => {
+    return (
+        <div>ModalTareas</div>
+    )
+}
